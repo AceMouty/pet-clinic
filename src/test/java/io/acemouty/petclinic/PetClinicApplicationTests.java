ab14317b208c4ba77663b91461a2e22120efe180
Kyle Guerrero <kyleguerrero85@gmail.com>
2021-10-29 01:33:27 -0500

    project init

---
+++ src/test/java/io/acemouty/petclinic/PetClinicApplicationTests.java
@@ -0,0 +1,15 @@
+package io.acemouty.petclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetClinicApplicationTests
+{
+
+  @Test
+  void contextLoads()
+  {
+  }
+
+}
